897a54320aee11230b74fe8c7fd67e03fd0a4bcb
jackson2610 <61340522+jackson2610@users.noreply.github.com>
2020-08-28 18:06:55 +0800

    Add files via upload

---
+++ voicerecognition.py
@@ -0,0 +1,50 @@
+import pyttsx3
+import speech_recognition
+from datetime import date, datetime
+import sys
+
+
+robot_ear=speech_recognition.Recognizer()
+robot_mouth= pyttsx3.init()
+robot_brain=""
+
+while True:
+
+    with speech_recognition.Microphone() as mic:
+        print("Robot: Password Authentication Please")
+        audio=robot_ear.listen(mic)
+
+    print("Robot:...")
+
+    try:
+        you=robot_ear.recognize_google(audio)
+    except:
+        you=""
+
+    print("You: " + you)
+
+
+    if you=="":
+        robot_brain="I can't hear you, try again"
+    elif 'hello' in you:
+        robot_brain="Congratulation, correct password"
+    elif "today" in you:
+        today = date.today()
+        robot_brain = today.strftime("%B %d, %Y")
+    elif "time" in you:
+        now = datetime.now()
+        robot_brain=now.strftime("%H hours %M minutes %S seconds")
+    elif "stop" in you:
+        robot_brain='Goodbye'
+        print("Robot:"+ robot_brain)
+        robot_mouth.say(robot_brain)
+        robot_mouth.runAndWait()
+        sys.exit()
+    else:
+        robot_brain="Wrong password, try again"
+
+    print("Robot:"+ robot_brain)
+    robot_mouth.say(robot_brain)
+    robot_mouth.runAndWait()
+
+
