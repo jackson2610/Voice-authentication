2ea69bf024eb55607604ed2d56fcef64ed2e701a
jackson2610 <61340522+jackson2610@users.noreply.github.com>
2020-09-01 15:18:08 +0800

    Update voicerecognition.py

--- voicerecognition.py
+++ voicerecognition.py
@@ -2,50 +2,52 @@ import pyttsx3
 import speech_recognition
 from datetime import date, datetime
 import sys
+open=1
+close=2
+message= int(input('input message:'))
+if message==1:
+    def voice_recognition():
+        robot_ear=speech_recognition.Recognizer()
+        robot_mouth= pyttsx3.init()
+        robot_brain=""
+        while True:
+            with speech_recognition.Microphone() as mic:
+                print("Robot: Password Authentication Please")
+                audio=robot_ear.listen(mic)
 
-def voice_recognition:
-    robot_ear=speech_recognition.Recognizer()
-    robot_mouth= pyttsx3.init()
-    robot_brain=""
+            print("Robot:...")
 
-    while True:
+            try:
+                you=robot_ear.recognize_google(audio)
+            except:
+                you=""
 
-        with speech_recognition.Microphone() as mic:
-            print("Robot: Password Authentication Please")
-            audio=robot_ear.listen(mic)
-
-        print("Robot:...")
-
-        try:
-            you=robot_ear.recognize_google(audio)
-        except:
-            you=""
-
-        print("You: " + you)
+            print("You: " + you)
 
 
-        if you=="":
-            robot_brain="I can't hear you, try again"
-        elif 'hello' in you:
-            robot_brain="Congratulation, correct password"
-        elif "today" in you:
-            today = date.today()
-            robot_brain = today.strftime("%B %d, %Y")
-        elif "time" in you:
-            now = datetime.now()
-            robot_brain=now.strftime("%H hours %M minutes %S seconds")
-        elif "stop" in you:
-            robot_brain='Goodbye'
+            if you=="":
+                robot_brain="I can't hear you, try again"
+            elif 'hello' in you:
+                robot_brain="Congratulation, correct password"
+                return True
+            elif "stop" in you:
+                robot_brain='Goodbye'
+                print("Robot:"+ robot_brain)
+                robot_mouth.say(robot_brain)
+                robot_mouth.runAndWait()
+                sys.exit()
+            else:
+                robot_brain="Wrong password, try again"
+                return False
+
             print("Robot:"+ robot_brain)
             robot_mouth.say(robot_brain)
             robot_mouth.runAndWait()
-            sys.exit()
-        else:
-            robot_brain="Wrong password, try again"
-
-        print("Robot:"+ robot_brain)
-        robot_mouth.say(robot_brain)
-        robot_mouth.runAndWait()
-voice_recognition()
-
-
+    voice_recognition()
+elif message == 2 :
+    print("system will be close right now.")
+    sys.exit()
+else :
+    print("Wrong")
+    return False
+    sys.exit()
