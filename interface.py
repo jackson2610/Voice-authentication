288043d8f385265e9eea46d5554975203f807dad
Judono <47880633+Judono@users.noreply.github.com>
2020-09-01 13:21:52 +0700

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import ImageTk, Image
+from VoiceRecognition import voice_recognition
+
+def voiceRec():
+    if voice_recognition() == False:
+        callback.after(100, voiceRec)
+
+    messagebox.showinfo("Success", "Login Successful")
+
+
+app = Tk()
+w = 500
+h = 500
+app.geometry('%dx%d+0+0' % (w,h))
+messagebox.showinfo("Info", "Welcome to JTech")
+
+user_Label = Label(app, text="Welcome To JTech", font= "Arial 40")
+user_Label.pack()
+
+
+btn1 = Button(app, text = "Voice Recognition", command = voiceRec)
+btn1.pack()
+
+btn2 = Button(app, text = "Facial Recognition")
+btn2.pack()
+
+app.mainloop()
